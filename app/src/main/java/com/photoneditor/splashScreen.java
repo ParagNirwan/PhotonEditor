e55d3e1be7365bee5ea25c4e4b6456896a5c8948
ParagNirwan <52133765+ParagNirwan@users.noreply.github.com>
2023-08-15 17:01:41 +0200

    Splash Screen

---
+++ app/src/main/java/com/photoneditor/splashScreen.java
@@ -0,0 +1,24 @@
+package com.photoneditor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(splashScreen.this,MainActivity.class));finish();
+            }
+        },3000);
+    }
+}
