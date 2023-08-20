da07c83f59b35c9a600a9b39c86a8254bd5cb2ad
ParagNirwan <52133765+ParagNirwan@users.noreply.github.com>
2023-08-20 15:32:47 +0200

    Editing Tools added

--- app/src/main/java/com/photoneditor/FinalActivity.java
+++ app/src/main/java/com/photoneditor/FinalActivity.java
@@ -2,11 +2,14 @@ package com.photoneditor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.dsphotoeditor.sdk.activity.DsPhotoEditorActivity;
+import com.dsphotoeditor.sdk.utils.DsPhotoEditorConstants;
 import com.photoneditor.databinding.ActivityFinalBinding;
 
 public class FinalActivity extends AppCompatActivity {
@@ -15,19 +18,33 @@ ActivityFinalBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding=ActivityFinalBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        Uri imageUri = getIntent().getParcelableExtra("imageUri");
-        if (imageUri != null) {
-            // Now you can use the imageUri as needed
-            // For example, display the image in an ImageView
-            try{
-                ImageView imageView = findViewById(R.id.imageView);
-                imageView.setImageURI(imageUri);
-            }
-            catch(Exception e){
-                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        Uri selectedImageUri = getIntent().getParcelableExtra("imageUri");
+        Intent dsPhotoEditorIntent = new Intent(this, DsPhotoEditorActivity.class);
+        dsPhotoEditorIntent.setData(selectedImageUri);
+        dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_OUTPUT_DIRECTORY, "Photon Editor");
+
+        startActivityForResult(dsPhotoEditorIntent, 200);
+        }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+
+            switch (requestCode) {
+
+                case 200:
+
+                    Uri outputUri = data.getData();
+
+                     binding.imageView3.setImageURI(outputUri);
+
+                    break;
+
             }
 
         }
+
     }
-}
+    }
